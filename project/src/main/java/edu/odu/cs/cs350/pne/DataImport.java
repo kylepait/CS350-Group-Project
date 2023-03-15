3f95522c5681cf41087abc8bc720e40a04706d89
mgees002 <mgees002@odu.edu>
2023-03-15 17:08:41 -0400

    Made changes to dataimport and added test

--- project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
@@ -1,20 +1,20 @@
 package edu.odu.cs.cs350.pne;
+
 import java.io.File;
 import java.io.IOException;
 
-public class FileImport
-{
+public class DataImport {
     public static void main(String[] args) throws IOException {
-        //Creating a File object for directory
+        // Creating a File object for directory
         File directoryPath = new File("../src/test/data/History/202010");
-        //List of all files and directories
+        // List of all files and directories
         File filesList[] = directoryPath.listFiles();
         System.out.println("List of files and directories in the specified directory:");
-        for(File file : filesList) {
-           System.out.println("File name: "+file.getName());
-           System.out.println("File path: "+file.getAbsolutePath());
-           System.out.println("Size :"+file.getTotalSpace());
-           System.out.println(" ");
+        for (File file : filesList) {
+            System.out.println("File name: " + file.getName());
+            System.out.println("File path: " + file.getAbsolutePath());
+            System.out.println("Size :" + file.getTotalSpace());
+            System.out.println(" ");
         }
     }
 }
---
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestDataImport.java
@@ -0,0 +1,19 @@
+package edu.odu.cs.cs350.pne;
+
+import org.junit.jupiter.api.Test;
+
+import edu.odu.cs.Interval;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Test of the Interval class
+ */
+public class TestDataImport {
+
+}
