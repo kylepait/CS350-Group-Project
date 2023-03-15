d11e6f7e57865ab9fbbffa413f889b61fb2168ec
dbhaggan <dbhag001@odu.edu>
2023-03-15 15:49:40 -0400

    semester java

--- project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
@@ -1,3 +1,45 @@
-public class Semester {
-    
+import java.util.List;
+
+public class Semester 
+{
+    private List<Offerings> offeringList;
+    private List<String> CRSEList;
+    private String semesterCode;
+
+    public Semester(List<Offering> offeringList, List<String> CRSEList, String semesterCode)
+    {
+        this.offeringList = offeringList;
+        this.CRSEList = CRSEList;
+        this.semesterCode = semesterCode;
+    }
+
+    public List<Offering> getOfferingList()
+    {
+        return offeringList
+    }
+
+    public void setOfferingList(List<Offering> offeringList)
+    {
+        this.offeringList = offeringList;
+    }
+
+    public List<String> getCRSEList()
+    {
+        return CRSEList;
+    }
+
+    public void setCRSEList(List<String> CRSEList)
+    {
+        this.CRSEList = CRSEList;
+    }
+
+    public String getSemesterCode()
+    {
+        return semesterCode;
+    }
+
+    public void SetSemesterCode(String semesterCode)
+    {
+        this.semesterCode = semesterCode;
+    }
 }
