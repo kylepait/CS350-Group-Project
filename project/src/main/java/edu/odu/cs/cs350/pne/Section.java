de30525372063f90a16d6106aa3014be9bb80ba7
kpait002 <kpait002@odu.edu>
2023-03-15 09:40:07 -0400

    production1 added empty construction to section class

--- project/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -1,3 +1,19 @@
 public class Section {
+
+    int crn;
+    int seatsRemaining;
+    int crossListCap;
+    int enrollments;
+    String instructor;
+    String link;
+
+    public Section(){
+        crn = 0;
+        seatsRemaining = 0;
+        crossListCap = 0;
+        enrollments = 0;
+        instructor = "";
+        line = "";
+    }
     
 }
