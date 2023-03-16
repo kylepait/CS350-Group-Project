f455878c9e0dc3713b53f2cdefcfd5acd0972ed4
mgees002 <mgees002@odu.edu>
2023-03-16 12:33:34 -0400

    Prints out contents of files in directory

--- project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
@@ -2,6 +2,7 @@ package edu.odu.cs.cs350.pne;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class DataImport {
     private File filesList[];
@@ -10,15 +11,24 @@ public class DataImport {
         filesList = null;
     }
 
-    public void GetFileNames(String FilePath) {
+    public void GetFileNames(String FilePath) throws IOException {
         // Creating a File object for directory
         File directoryPath = new File(FilePath);
         // List of all files and directories
         filesList = directoryPath.listFiles();
         System.out.println("List of files and directories in the specified directory:");
+        Scanner ScannerContents = null;
         for (File file : filesList) {
             System.out.println("File name: " + file.getName());
             System.out.println("File path: " + file.getAbsolutePath());
+            ScannerContents = new Scanner(file);
+            String input;
+            StringBuffer FileContents = new StringBuffer();
+            while (ScannerContents.hasNextLine()) {
+                input = ScannerContents.nextLine();
+                FileContents.append(input + " ");
+            }
+            System.out.println("Contents of the file: " + FileContents.toString());
             System.out.println(" ");
         }
     }
