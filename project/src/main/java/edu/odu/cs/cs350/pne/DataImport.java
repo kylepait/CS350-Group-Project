12277148091c3d7ccf11dea0893e16012c4ef938
mgees002 <mgees002@odu.edu>
2023-03-16 13:58:13 -0400

    Parses date from filename

--- project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
@@ -3,6 +3,8 @@ package edu.odu.cs.cs350.pne;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import org.apache.commons.io.FilenameUtils;
+import java.time.LocalDate;
 
 public class DataImport {
     private File filesList[];
@@ -14,12 +16,23 @@ public class DataImport {
     public void GetFileNames(String FilePath) throws IOException {
         // Creating a File object for directory
         File directoryPath = new File(FilePath);
+        History history = new History();
         // List of all files and directories
         filesList = directoryPath.listFiles();
         System.out.println("List of files and directories in the specified directory:");
         Scanner ScannerContents = null;
         for (File file : filesList) {
             System.out.println("File name: " + file.getName());
+            String Filename = FilenameUtils.removeExtension(file.getName());
+            String[] FilenameParts = Filename.split("-");
+            for (String Date : FilenameParts) {
+                System.out.println("Date Parts: " + Date);
+            }
+            if (FilenameParts.length == 3) {
+                LocalDate Date = LocalDate.of(Integer.parseInt(FilenameParts[0]), Integer.parseInt(FilenameParts[1]),
+                        Integer.parseInt(FilenameParts[2]));
+                System.out.println("Date : " + Date);
+            }
             System.out.println("File path: " + file.getAbsolutePath());
             ScannerContents = new Scanner(file);
             String input;
@@ -28,7 +41,7 @@ public class DataImport {
                 input = ScannerContents.nextLine();
                 FileContents.append(input + " ");
             }
-            System.out.println("Contents of the file: " + FileContents.toString());
+            // System.out.println("Contents of the file: " + FileContents.toString());
             System.out.println(" ");
         }
     }
--- project/src/main/java/edu/odu/cs/cs350/pne/Main.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Main.java
@@ -1,8 +1,10 @@
 package edu.odu.cs.cs350.pne;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DataImport instance = new DataImport();
         if (args[0] == null || args[0].trim().isEmpty()) {
             System.out.println("You need to specify a path!");
