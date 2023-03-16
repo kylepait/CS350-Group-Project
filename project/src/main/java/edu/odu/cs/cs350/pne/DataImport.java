2cb01a26031e9be5d373930783bdd9987f055cfa
mgees002 <mgees002@odu.edu>
2023-03-16 12:08:38 -0400

    Program can be passed directory and contents print

--- project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/DataImport.java
@@ -4,16 +4,21 @@ import java.io.File;
 import java.io.IOException;
 
 public class DataImport {
-    public static void main(String[] args) throws IOException {
+    private File filesList[];
+
+    DataImport() {
+        filesList = null;
+    }
+
+    public void GetFileNames(String FilePath) {
         // Creating a File object for directory
-        File directoryPath = new File("../src/test/data/History/202010");
+        File directoryPath = new File(FilePath);
         // List of all files and directories
-        File filesList[] = directoryPath.listFiles();
+        filesList = directoryPath.listFiles();
         System.out.println("List of files and directories in the specified directory:");
         for (File file : filesList) {
             System.out.println("File name: " + file.getName());
             System.out.println("File path: " + file.getAbsolutePath());
-            System.out.println("Size :" + file.getTotalSpace());
             System.out.println(" ");
         }
     }
--- project/src/main/java/edu/odu/cs/cs350/pne/Main.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Main.java
@@ -1,9 +1,14 @@
 package edu.odu.cs.cs350.pne;
 
 public class Main {
-    
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        DataImport instance = new DataImport();
+        if (args[0] == null || args[0].trim().isEmpty()) {
+            System.out.println("You need to specify a path!");
+            return;
+        } else {
+            instance.GetFileNames(args[0]);
+        }
     }
 }
