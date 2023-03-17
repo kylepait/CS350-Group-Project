c245b812152bf1f59bf6b0f6a4628ad421072be1
Xavier Jordan <xjord002@odu.edu>
2023-03-17 09:27:27 -0400

    Made tests for maxenrollment and currentenrollment

--- project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -3,14 +3,10 @@ package edu.odu.cs.cs350.pne;
 import java.util.List;
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.beans.Transient;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 public class TestOffering {
     @Test
@@ -28,14 +24,20 @@ public class TestOffering {
     @Test
     public void testDestructor() {
         List<Section> section = new ArrayList<Section>();
-        Offering offering = new Offering(section, "350", "CS");
+        Offering offering = new Offering(section, "350", "CS", 5, 20, 15);
         offering.setSection(section);
         offering.setCRSE("350");
         offering.setSUBJ("CS");
+        offering.setEnrollment(5);
+        offering.setMaxEnrollment(20);
+        offering.setCurrentEnrollment(15);
 
         assertEquals(section, offering.getSection());
         assertEquals("350", offering.getCRSE());
         assertEquals("CS", offering.getSUBJ());
+        assertEquals(5, offering.getEnrollment());
+        assertEquals(20, offering.getMaxEnrollment());
+        assertEquals(15, offering.getCurrentEnrollment());
     }
 
     @Test
@@ -102,5 +104,37 @@ public class TestOffering {
         offering.enrollment = 10;
 
         assertEquals(10, offering.getEnrollment());
+    }
+
+    @Test
+    public void testSetMaxEnrollment() {
+        Offering offering = new Offering();
+        offering.setMaxEnrollment(30);
+
+        assertEquals(30, offering.getMaxEnrollment());
+    }
+
+    @Test
+    public void testGetMaxEnrollment() {
+        Offering offering = new Offering();
+        offering.maxEnrollment = 40;
+
+        assertEquals(40, offering.getMaxEnrollment());
+    }
+
+    @Test
+    public void testSetCurrentEnrollment() {
+        Offering offering = new Offering();
+        offering.setCurrentEnrollment(32);
+
+        assertEquals(32, offering.getCurrentEnrollment());
+    }
+
+    @Test
+    public void testGetCurrentEnrollment() {
+        Offering offering = new Offering();
+        offering.currentEnrollment = 45;
+
+        assertEquals(45, offering.getCurrentEnrollment());
     }
 }
