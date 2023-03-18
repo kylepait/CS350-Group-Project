de004ccd6f37594aae45f2dbbffd73756b83d986
mgees002 <mgees002@odu.edu>
2023-03-18 12:10:21 -0400

    Minor changes

--- project/src/test/java/edu/odu/cs/cs350/pne/TestMain.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestMain.java
@@ -14,38 +14,42 @@ import java.nio.file.Paths;
 
 public class TestMain {
     @Test
-    public void TestGetFileContents() throws IOException {
+    public void TestGetFileContentsNoDataTxt() {
+        String path = "src/test/data/History/202010";
+        File dataDirectory = new File(path);
         ArrayList<History> PreviousSemestersData;
         PreviousSemestersData = new ArrayList<History>();
-        /*
-         * PreviousSemestersData.add(new
-         * Main().GetFileContents("./data/History/202010"));
-         * for (History hist : PreviousSemestersData) {
-         * // System.out.println(hist.getSemester());
-         * for (int i = 0; i < hist.getSemester().size(); i++) {
-         * Semester semes = hist.getSemesterByIndex(i);
-         * System.out.println(semes.getSemesterCode());
-         * System.out.println(hist.getSnapShotByIndex(i));
-         * // System.out.println(semes);
-         * List<Offering> Off = semes.getOfferingList();
-         * List<String> CRSELst = semes.getCRSEList();
-         * for (int j = 0; j < Off.size(); j++) {
-         * // System.out.println(CRSELst.get(j));
-         * System.out.println("    CRSE: " + Off.get(j).getCRSE() + " SUBJ: " +
-         * Off.get(j).getSUBJ()
-         * + " ENRL: " + Off.get(j).getEnrollment() + " MaxENRL: " +
-         * Off.get(j).getMaxEnrollment()
-         * + " CurrENRL: " + Off.get(j).getCurrentEnrollment());
-         * for (Section sect : Off.get(j).getSection()) {
-         * System.out.println(
-         * "        CRN: " + sect.getCRN() + " Seats Remaining: " +
-         * sect.getSeatsRemaining()
-         * + " XList Cap: " + sect.getCrossListCap() + " ENRL:" + sect.getEnrollments()
-         * + " Instr: " + sect.getInstructor() + " Link: " + sect.getLink());
-         * }
-         * }
-         * }
-         * }
-         */
+        try {
+            PreviousSemestersData.add(new Main().GetFileContents(dataDirectory.getAbsolutePath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (History hist : PreviousSemestersData) {
+            // System.out.println(hist.getSemester());
+            for (int i = 0; i < hist.getSemester().size(); i++) {
+                Semester semes = hist.getSemesterByIndex(i);
+                System.out.println(semes.getSemesterCode());
+                System.out.println(hist.getSnapShotByIndex(i));
+                // System.out.println(semes);
+                List<Offering> Off = semes.getOfferingList();
+                List<String> CRSELst = semes.getCRSEList();
+                for (int j = 0; j < Off.size(); j++) {
+                    // System.out.println(CRSELst.get(j));
+                    System.out.println("    CRSE: " + Off.get(j).getCRSE() + " SUBJ: " +
+                            Off.get(j).getSUBJ()
+                            + " ENRL: " + Off.get(j).getEnrollment() + " MaxENRL: " +
+                            Off.get(j).getMaxEnrollment()
+                            + " CurrENRL: " + Off.get(j).getCurrentEnrollment());
+                    for (Section sect : Off.get(j).getSection()) {
+                        System.out.println(
+                                "        CRN: " + sect.getCRN() + " Seats Remaining: " +
+                                        sect.getSeatsRemaining()
+                                        + " XList Cap: " + sect.getCrossListCap() + " ENRL:" + sect.getEnrollments()
+                                        + " XList Group: " + sect.getCrossListGroup() + " Instr: "
+                                        + sect.getInstructor() + " Link: " + sect.getLink());
+                    }
+                }
+            }
+        }
     }
 }
