8ba5fe2ec5b128802d369e51c2340277d3d244ce
mgees002 <mgees002@odu.edu>
2023-03-18 15:00:28 -0400

    Added tests

--- project/src/test/java/edu/odu/cs/cs350/pne/TestMain.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestMain.java
@@ -12,13 +12,19 @@ import java.io.File;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class TestMain {
     @Test
     public void TestGetFileContentsNoDataTxt() {
         String path = "src/test/data/History/199010";
         File dataDirectory = new File(path);
-        ArrayList<History> PreviousSemestersData;
         IOException ThrownMessage;
         Exception exception = assertThrows(IOException.class, () -> {
             new Main().GetFileContents(dataDirectory.getAbsolutePath());
@@ -34,7 +40,6 @@ public class TestMain {
     public void TestGetFileContentsNotEnoughSnapshots() {
         String path = "src/test/data/History/199020";
         File dataDirectory = new File(path);
-        ArrayList<History> PreviousSemestersData;
         IOException ThrownMessage;
         Exception exception = assertThrows(IOException.class, () -> {
             new Main().GetFileContents(dataDirectory.getAbsolutePath());
@@ -45,4 +50,54 @@ public class TestMain {
 
         assertThat(actualMessage, equalTo(expectedMessage));
     }
+
+    @Test
+    public void TestGetFileNumSections() {
+        String path = "src/test/data/History/199020";
+        String path2 = "src/test/data/History/199020/2020-04-01.csv";
+        File dataDirectory = new File(path);
+        File dataFile = new File(path2);
+        File filesList[];
+        int Sections = 0, SectionsFromFile = 0;
+        ArrayList<History> PreviousSemestersData;
+        PreviousSemestersData = new ArrayList<History>();
+        try {
+            PreviousSemestersData.add(new Main().GetFileContents(dataDirectory.getAbsolutePath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        History hist = PreviousSemestersData.get(0);
+        Semester semes = hist.getSemesterByIndex(0);
+        // System.out.println(semes);
+        List<Offering> Off = semes.getOfferingList();
+        for (int j = 0; j < Off.size(); j++) {
+            // System.out.println(CRSELst.get(j));
+            for (Section sect : Off.get(j).getSection()) {
+                Sections++;
+            }
+        }
+        try {
+            File file = dataFile;
+            // Get date of snapshot from filename.
+            Reader CSVFile = Files.newBufferedReader(Paths.get(file.getPath()));
+            CSVParser parser = CSVParser.parse(CSVFile, CSVFormat.RFC4180);
+            List<CSVRecord> csvRecords = parser.getRecords();
+            for (int i = 1; i < csvRecords.size(); i++) {
+                CSVRecord csvRecord = csvRecords.get(i);
+                String Link = csvRecord.get(8);
+                if (Link.length() == 2) {
+                    if (Link.charAt(1) == '1') {
+                        SectionsFromFile++;
+                    }
+                } else {
+                    SectionsFromFile++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertThat(Sections, equalTo(SectionsFromFile));
+    }
+
 }
