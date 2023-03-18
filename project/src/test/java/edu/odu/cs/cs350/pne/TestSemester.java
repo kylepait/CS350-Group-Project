be7139e6864e8dfa7baad2b232ba521eba98ca6e
dbhaggan <dbhag001@odu.edu>
2023-03-17 21:39:23 -0400

    testSemester4

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
@@ -21,23 +21,23 @@ public class TestSemester
     public void testGetOfferingList()
     {
         List<Offering> offeringList = new ArrayList<>();
-        offeringList.add(new Offering("CS101"));
+        offeringList.add(new Offering(new ArrayList<>(), "CS101", "COMPSCI", 0, 0, 0));
 
         Semester semester = new Semester(offeringList, new ArrayList<>(), "SP21");
 
-        assertEquals(offeringList, semester.testGetOfferingList());
+        assertEquals(offeringList, semester.getOfferingList());
     }
 
     @Test
     public void testSetOfferingList()
     {
         List<Offering> offeringList1 = new ArrayList<>();
-        offeringList1.add(new Offering("CS101"));
+        offeringList1.add(new Offering(new ArrayList<>(), "CS101", "COMPSCI", 0, 0, 0));
 
         Semester semester = new Semester(offeringList1, new ArrayList<>(), "SP21");
 
         List<Offering> offeringList2 = new ArrayList<>();
-        offeringList2.add(new Offering("CS102"));
+        offeringList2.add(new Offering(new ArrayList<>(), "CS102", "COMPSCI", 0, 0, 0));
 
         semester.setOfferingList(offeringList2);
 
@@ -50,7 +50,7 @@ public class TestSemester
         List<String> CRSEList = new ArrayList<>();
         CRSEList.add("CS101");
 
-        Semester semester = new Semester(new ArrayList<>(), CRSEList1, "SP21");
+        Semester semester = new Semester(new ArrayList<>(), CRSEList, "SP21");
 
         assertEquals(CRSEList, semester.getCRSEList());
     }
@@ -66,7 +66,7 @@ public class TestSemester
         List<String> CRSEList2 = new ArrayList<>();
         CRSEList2.add("CS102");
 
-        semester.setCrseList(CRSEList2);
+        semester.setCRSEList(CRSEList2);
 
         assertEquals(CRSEList2, semester.getCRSEList());
     }
@@ -78,7 +78,7 @@ public class TestSemester
 
         Semester semester = new Semester(new ArrayList<>(), new ArrayList<>(), semesterCode);
 
-        assertEquals(semesterCode, semester.GetSemesterCode());
+        assertEquals(semesterCode, semester.getSemesterCode());
     }
 
     @Test
@@ -86,12 +86,12 @@ public class TestSemester
     {
         String semesterCode1 = "SP21";
 
-        Semester semester = new Semester(new ArrayList<>(), new ArrayList<>(), semseterCode1);
+        Semester semester = new Semester(new ArrayList<>(), new ArrayList<>(), semesterCode1);
 
         String semesterCode2 = "FA21";
 
         semester.SetSemesterCode(semesterCode2);
 
-        assertEquals(semesterCode2, semester.getSemesterCode()0);
+        assertEquals(semesterCode2, semester.getSemesterCode());
     }
 }
