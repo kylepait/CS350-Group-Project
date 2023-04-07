3ce394935650d758fa70de6d094fdebbe07e3e4f
mgees002 <mgees002@odu.edu>
2023-04-07 15:51:03 -0400

    Added headers

--- project/src/main/java/edu/odu/cs/cs350/pne/Output.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Output.java
@@ -19,9 +19,12 @@ public class Output {
     private static final String SHEETNAME = "output";
     private static final String[] HEADERS = { "Column 1", "Column 2", "Column 3" };
 
-    public static void outputToTxt(List<List<String>> data, String Filename) throws IOException {
+    public static void outputToTxt(String Header1, String Header2, List<List<String>> data, String Filename)
+            throws IOException {
         File file = new File(Filename + ".txt");
         try (PrintWriter writer = new PrintWriter(file)) {
+            writer.println(Header1);
+            writer.println(Header2);
             for (List<String> line : data) {
                 writer.printf("%-7s%-11s%-10s%-3s\n", line.get(0), line.get(1), line.get(2), line.get(3));
             }
