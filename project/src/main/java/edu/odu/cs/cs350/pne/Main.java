861e53529d54cdea8c4d81327f2d75b8cdd786fa
abasy001 <abasy001@odu.edu>
2023-04-23 14:50:24 -0400

    interpolate2

--- project/src/main/java/edu/odu/cs/cs350/pne/Main.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Main.java
@@ -433,15 +433,15 @@ public class Main {
         return totalEnrollment;
     }
 
-    public Integer Interpolate(double startPoint, Integer startEnrollment, double endPoint, 
-    Integer endEnrollment, double projectionPoint){
+    public int Interpolate(double startPoint, int startEnrollment, double endPoint,
+            int endEnrollment, double projectionPoint) {
 
-        Integer projectedEnrollment;    //value to be returned
-        double ARC = (endEnrollment-startEnrollment)/(endPoint-startPoint);     //avg rate of change (slope)
-        double timeSinceStart = projectionPoint-startPoint;                     
-        double projec = startEnrollment + (ARC*timeSinceStart);       //interpolation equation
+        int projectedEnrollment; // value to be returned
+        double ARC = (endEnrollment - startEnrollment) / (endPoint - startPoint); // avg rate of change (slope)
+        double timeSinceStart = projectionPoint - startPoint;
+        double projec = startEnrollment + (ARC * timeSinceStart); // interpolation equation
         projectedEnrollment = (int) Math.round(projec);
-        
+
         return projectedEnrollment;
     }
 }
